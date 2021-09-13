022198a6ac6a0ba1e8e2516c4e89819c9fb33f25
bahau88 <84557850+bahau88@users.noreply.github.com>
2021-09-13 06:03:14 +0300

    Add files via upload

---
+++ src/task2.py
@@ -0,0 +1,24 @@
+import contextlib 
+import time, io
+import inspect
+from contextlib import 
+
+def decorator_2(f):
+    def wrapper(*args, **kwargs):
+        wrapper.count+=1
+        with contextlib.redirect_stdout(io.StringIO()) as f_:
+            f(*args, **kwargs)
+            start = perf_counter()
+            end = perf_counter()
+        out = f.getvalue()
+        print(f"{f.__name__} call {wrapper.count} executed in {end-start} sec") + ' sec')
+        print('Name: '+func.__name__)
+        print('Type: '+str(type(func)))
+        print('Sign: '+str(inspect.signature(func)))
+        print('Args: ' + str(args['args']))
+        print('Doc: '+str(func.__doc__))
+        print('Source: '+str(inspect.getsource(func)))
+        print('Outputs: ', s)
+        print('function outputs: ', out,"\n")
+    return wrapper
+        
